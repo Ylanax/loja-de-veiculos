57f4f699d671cca10a0e399fa511da22723eb06d
Ylana Pereira <ylana.pereira2004@outlook.com>
2025-06-26 12:51:20 -0300

    tentativa 1

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,15 +9,50 @@
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
+    <header>
+        <img src="img/logo.jpg" class="logo" height="150">
+        <h1 class="logo-texto">DriveWay</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="">Automóveis</a>
+            <a href=""></a>
+            <a href="contato.php">Contato</a>
+        </nav>
+    </header>
+    <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="imagens/mercedes-prata.jpg" class="d-block w-100 banner" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="imagens/mercedes-preta.jpg" class="d-block w-100 banner" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="imagens/mercedes-vermelha.jpg" class="d-block w-100 banner" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="imagens/lamborghini.jpg" class="d-block w-100 banner" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
 
 
 
 
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
         crossorigin="anonymous"></script>
 </body>
+
 </html>
