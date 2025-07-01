68fcc724d01f4c018d74172d1ec5ba405c04d718
Ylana Pereira <ylana.pereira2004@outlook.com>
2025-06-30 22:25:37 -0300

    att

--- contato.php
+++ contato.php
@@ -3,7 +3,7 @@ include "cabecalho.php";
 ?>
 
 <div class="form-container">
-        <h2>Fale com a gente</h2>
+        <h2>Entre em contato com nossos vendedores!</h2>
         <form action="resposta.php" method="post">
             <label for="nome">Nome</label>
             <input type="text" id="nome" name="nome" placeholder="Seu nome completo" required>
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 include "cabecalho.php";
-?>
-
-<?php
 include "banner.php";
 ?>
 
@@ -35,7 +32,7 @@ include "banner.php";
     </div>
     <div class="row">
         <div class="col-6">
-            <img src="imagens/bmw.jpg" class="img-fluid">
+            <img src="imagens/bmw.jpg" class="img-fluid mt-5">
         </div>
         <div class="col-6 align-content-center">
             <h2 class="bmw-m4 fw-bold text-black">BMW M4 Coupé</h2>
@@ -44,7 +41,7 @@ include "banner.php";
             </p>
             <ul class="fw-semibold">
                 <li>Motor: 3.0 litros, 6 cilindros em linha, TwinPower Turbo (biturbo)</li>
-                <li>>Potência: Versão padrão: 431 cv (425 hp) @ 5.500–7.300 rpm.</li>
+                <li>Potência: Versão padrão: 431 cv (425 hp) @ 5.500–7.300 rpm.</li>
                 <li>Torque: 550 Nm (406 lb-ft) @ 1.850–5.500 rpm.</li>
                 <li>Transmissão: Manual de 6 marchas / Automática de dupla embreagem (DCT) de 7 marchas (opcional).</li>
                 <li>Performance (M4 padrão): 0–100 km/h: ~4,1 segundos (com DCT) // Velocidade máxima: 250 km/h (limitada eletronicamente).</li>
@@ -56,9 +53,9 @@ include "banner.php";
     <div class="row">
         <div class="col-6 align-content-center">
             <h2 class="fw-bold text-black">Ferrari 458 Speciale</h2>
-            <p class="fw-semibold text-black ms-2">Esse modelo é muito valorizado por colecionadores, pois é um dos últimos V8 aspirados da Ferrari antes da chegada dos motores turbo com a 488 GTB.
+            <p class="fw-semibold text-black ms-4">Esse modelo é muito valorizado por colecionadores, pois é um dos últimos V8 aspirados da Ferrari antes da chegada dos motores turbo com a 488 GTB.
             </p>
-            <ul class="fw-semibold">
+            <ul class="fw-semibold ms-4">
                 <li>Produção: 2013–2015</li>
                 <li>Motor: 4.5 litros V8 aspirado (sem turbo)</li>
                 <li>Potência: 605 cv (597 hp) @ 9.000 rpm</li>
@@ -69,7 +66,7 @@ include "banner.php";
                 <li>Transmissão: Automática de dupla embreagem, 7 marchas</li>
                 <li>Tração: Traseira (RWD)</li>
             </ul>
-            <a href="https://www.ferrari.com/en-EN/search?search=ferrari%20408%20speciale&curr=0%7C0&active=0&limit=15%7C15&order=default%7Cdefault&lang=en%7Cen" class="btn btn-light fw-semibold ms-3">Saiba Mais</a>
+            <a href="https://www.ferrari.com/en-EN/search?search=ferrari%20408%20speciale&curr=0%7C0&active=0&limit=15%7C15&order=default%7Cdefault&lang=en%7Cen" class="btn btn-light fw-semibold ms-4">Saiba Mais</a>
         </div>
         <div class="col-6 mt-5">
             <img src="imagens/ferrari.jpg" class="img-fluid">
@@ -78,9 +75,4 @@ include "banner.php";
 
     <?php include "rodape.php"; ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
-        crossorigin="anonymous"></script>
-    </body>
-
-    </html>
+    
